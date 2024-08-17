e04a8f63b12c61d45d64f18265a1eb20a3294c6c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-17 09:46:40 -0300

    Create Routes file

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {path: "", element: <Home />}
+        ]
+    }
+])
