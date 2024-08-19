6316422a3525a2d94e3713494228a1f770ce07a4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-19 09:19:17 -0300

    Add path to my routes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
-            {path: "", element: <Home />}
+            {path: "", element: <Home />},
+            {path: "login", element: <Login />},
+            {path: "register", element: <Register />},
         ]
     }
 ])
