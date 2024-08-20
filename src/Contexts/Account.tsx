d75d0c0f6c247296e180990fca7aff934690fb65
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-20 10:35:46 -0300

    Create some functions and variables

--- src/Contexts/Account.tsx
+++ src/Contexts/Account.tsx
@@ -1,13 +1,20 @@
 import React, { createContext, useState } from "react";
+import { LoginType } from "../@Types/LoginType";
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({children} : {children: React.ReactNode}) =>{
     
-    const [userName, setUserName] = useState<string>('');
+    const [isLogged, setIsLogged] = useState<boolean>(false);
+    const [user, setUser] = useState<LoginType>();
 
     return(
-        <AuthContext.Provider value={{userName, setUserName}}>
+        <AuthContext.Provider value={{
+                    user,
+                    setUser,
+                    isLogged,
+                    setIsLogged
+                }}>
             <>{children}</>
         </AuthContext.Provider>
     )
