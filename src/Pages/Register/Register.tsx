23baf0fa06b349a01cd559871f57030a78b41bc1
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-20 10:39:07 -0300

    Create page Login

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
+import InputText from "../../Components/InputText/InputText";
+import { LabelSelect, LinkContainer, LoginContainer, SelectList } from "./Register.styles";
+import Button from "../../Components/Button/Button";
 
 const Register = () =>{
     return(
-        <div>
-            <input type="text" name="username" id="username" />
-            <input type="email" name="email" id="email" />
-            <input type="password" name="password" id="password" />
-            <input type="passwordAgain" name="passwordAgain" id="passwordAgain" />
-            <button>Register</button>
-            <Link to="/login">Already have an account?</Link>
-        </div>
+        <LoginContainer>
+            <InputText typeInput="text" placeText="example name" textLabel="Username" nameLabel="username" />
+            <InputText typeInput="email" placeText="exampleemail@example.com" textLabel="Email" nameLabel="email" />
+            <InputText typeInput="password" placeText="Strong password" textLabel="Password" nameLabel="password" />
+            <div className="SelectContainer">
+                <LabelSelect>Interest</LabelSelect>
+                <SelectList>
+                    <option className="optionItem">I want to hire!</option>
+                    <option className="optionItem">I want to work!</option>
+                </SelectList>
+            </div>
+            <Button textButton="Register"/>
+            <LinkContainer>
+                <Link to="/login" className="linkButton">Already have an account? Click Here!</Link>
+            </LinkContainer>
+        </LoginContainer>
     )
 }
 
