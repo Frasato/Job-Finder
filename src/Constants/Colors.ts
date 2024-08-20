4d2453fb71d1dc414a7c8d0a516df27e663d4947
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-20 10:37:02 -0300

    Create white color variable

---
+++ src/Constants/Colors.ts
@@ -0,0 +1,15 @@
+const primaryColor: string = "#3D5A73";
+const secundaryColor: string = "#28403D";
+const bgColor: string = "#182625";
+const lightGrayColors: string = "#455559";
+const darkGrayColor: string = "#2F3D40";
+const whiteColor: string = "white";
+
+export {
+    primaryColor,
+    secundaryColor,
+    bgColor,
+    lightGrayColors,
+    darkGrayColor,
+    whiteColor,
+}
