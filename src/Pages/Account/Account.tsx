1526786105b6f671d632973dc3ac5cecb219cdd3
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-21 14:34:29 -0300

    Init Account page

---
+++ src/Pages/Account/Account.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../../Components/NavBar/NavBar";
+
+const Account = () =>{
+    return(
+        <>
+            <NavBar />
+            <h1>Account!</h1>
+        </>
+    )
+}
+
+export default Account;
