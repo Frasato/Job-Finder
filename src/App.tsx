5d1edf7ed4fe352134af854726a0c43c27b63e15
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-21 12:55:49 -0300

    Toast dependences add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router"
 import AuthProvider from "./Contexts/Account"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <AuthProvider>
+      <ToastContainer autoClose={3000}/>
       <Outlet />
     </AuthProvider>
   )
