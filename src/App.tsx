2ec99d03eda2dbb68a7e7d22eeefc653f26abc01
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-22 08:54:33 -0300

    VacanciesContext Provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router"
 import AuthProvider from "./Contexts/Account"
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import VacanciesProvider from "./Contexts/VacanciesContext";
 
 function App() {
   return (
     <AuthProvider>
-      <ToastContainer autoClose={3000}/>
-      <Outlet />
+      <VacanciesProvider>
+        <ToastContainer autoClose={3000}/>
+        <Outlet />
+      </VacanciesProvider>
     </AuthProvider>
   )
 }
