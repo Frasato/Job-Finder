b499fadd519ee1131c7d26b9383c5af57407bb05
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-22 08:38:53 -0300

    Verify if exist user and login

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -2,13 +2,59 @@ import { Link } from "react-router-dom";
 import InputText from "../../Components/InputText/InputText";
 import { LinkContainer, LoginContainer } from "./Login.styles";
 import Button from "../../Components/Button/Button";
+import { useContext, useState } from "react";
+import { db } from "../../firebase/firebase";
+import { get, ref } from "firebase/database";
+import { Context } from "../../Contexts/Account";
+import { toast } from "react-toastify";
 
 const Login = () => {
+
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const {setIsLogged, setUser} = useContext(Context);
+
+    const handleUserName = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        const userNameLoweCase = event.target.value;
+        setUsername(userNameLoweCase.toLowerCase());
+    }
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        const passwordState = event.target.value;
+        setPassword(passwordState);
+    }
+
+    const handleLogin = async() =>{
+        try{
+            const dbRef = ref(db, `/users/${username}`);
+            const response = await get(dbRef);
+    
+            if(response.exists()){
+                const userData = response.val()
+                if(userData.password === password){
+                    setIsLogged(true);
+                    setUser({
+                        username: userData.username,
+                        email: userData.email,
+                        password: userData.password,
+                        interest: userData.interest,
+                    });
+                }else{
+                    toast.error("Invalid Password");
+                }
+            }else{
+                toast.warn("User doesn't exist");
+            }
+        }catch(error){
+            console.error(error);
+        }
+    }
+
     return(
         <LoginContainer>
-            <InputText typeInput="email" placeText="exampleemail@example.com" textLabel="Email" nameLabel="email" />
-            <InputText typeInput="password" placeText="Password..." textLabel="Password" nameLabel="password" />
-            <Button textButton="Login"/>
+            <InputText typeInput="text" placeText="Your Username" textLabel="Username" nameLabel="username" valueInput={username} onChangeInput={handleUserName}/>
+            <InputText typeInput="password" placeText="Password..." textLabel="Password" nameLabel="password" valueInput={password} onChangeInput={handlePassword}/>
+            <Button textButton="Login" onClick={handleLogin}/>
             <LinkContainer>
                 <Link to="/register">Do not have an account? Click here!</Link>
             </LinkContainer>
