51c07f205cb94078cae3f84292e896912989915f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-22 08:44:14 -0300

    Create Account page

---
+++ src/Pages/Account/Account.styles.ts
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const VacanciesContainerWork = styled.div``;
+
+export const VacanciesContainerHiring = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
--- src/Pages/Account/Account.tsx
+++ src/Pages/Account/Account.tsx
@@ -1,10 +1,39 @@
+import { useContext, useEffect, useState } from "react";
 import NavBar from "../../Components/NavBar/NavBar";
+import { Context } from "../../Contexts/Account";
+import { VacanciesContainerHiring, VacanciesContainerWork } from "./Account.styles";
+import Button from "../../Components/Button/Button";
+import { Navigate } from "react-router";
+import Separator from "../../Components/Separator/Separator";
 
 const Account = () =>{
+
+    const {interest} = useContext(Context);
+    const [btnClick, setBtnClick] = useState<boolean>(false);
+
+    useEffect(()=>{
+        setBtnClick(false);
+    },[])
+
+    if(btnClick){
+        return <Navigate to="/create"/>
+    }
+
     return(
         <>
             <NavBar />
-            <h1>Account!</h1>
+            {interest === 'Work' &&
+                <VacanciesContainerWork>
+                    {/* Vagas Aplicadas */}
+                </VacanciesContainerWork>
+            }
+            {interest === 'Hire' &&
+                <VacanciesContainerHiring>
+                    <Button textButton="Create" onClick={() => setBtnClick(true)}/>
+                        <Separator />
+                    {/* Vagas que a empresa tem em aberto */}
+                </VacanciesContainerHiring>
+            }
         </>
     )
 }
