698002d6fcebbb214609371cac8af91e58ad9616
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-23 15:43:47 -0300

    Create Logout Route

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import Account from "../Pages/Account/Account";
+import Logout from "../Pages/Logout/Logout";
 
 export const router = createBrowserRouter([
     {
@@ -14,7 +15,7 @@ export const router = createBrowserRouter([
             {path: "login", element: <Login />},
             {path: "register", element: <Register />},
             {path: "account", element: <Account />},
-            {path: "logout", element: <Account />},
+            {path: "logout", element: <Logout />},
         ]
     }
 ])
