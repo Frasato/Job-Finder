7575156fa40099de1a75bfa3c2a6800941dff2ef
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-23 15:53:30 -0300

    Clear Context and Redirect to home

--- src/Pages/Logout/Logout.tsx
+++ src/Pages/Logout/Logout.tsx
@@ -1,7 +1,32 @@
 import { Container, Icon, TextDisconect } from './Logout.styles';
 import icon from '../../Assets/icon.png';
+import { useContext, useEffect, useState } from 'react';
+import { Context } from '../../Contexts/Account';
+import { Navigate } from 'react-router';
 
 const Logout = () =>{
+
+    const {setUser, setIsLogged, setInterest} = useContext(Context);
+    const [redirect, setRedirect] = useState<boolean>(false);
+
+    useEffect(()=>{
+        setUser({
+            username: "",
+            email: "",
+            password: "",
+            interest: "",
+        });
+        setInterest("");
+        setIsLogged(false);
+        setTimeout(()=>{
+            setRedirect(true);
+        }, 5500);
+    },[])
+
+    if(redirect){
+        return <Navigate to="/"/>
+    }
+
     return(
         <Container>
             <Icon src={icon}/>
