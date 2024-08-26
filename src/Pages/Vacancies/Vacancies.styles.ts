314ae7404f61dd3a01e3dde302358976f49a7619
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-26 11:12:02 -0300

    Responsiviti elements

--- src/Pages/Vacancies/Vacancies.styles.ts
+++ src/Pages/Vacancies/Vacancies.styles.ts
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 import * as Colors from "../../Constants/Colors";
 
 export const VacanciesBody = styled.div`
-    height: 100vh;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -10,16 +10,36 @@ export const VacanciesBody = styled.div`
 
 export const ContentBody = styled.div`
     margin: 60px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const ImageContainer = styled.div`
-    width: 100%;
+    width: 330px;
     height: 200px;
+    object-fit: cover;
     overflow: hidden;
     display: flex;
     justify-content: center;
     margin-bottom: 20px;
-    margin-top: -191px;
+    border-radius: 10px;
+
+    @media screen and (min-width: 768px){
+        height: 258px;
+        width: 550px;
+    }
+
+    @media screen and (min-width: 1024px){
+        width: 780px;
+        height: 228px;
+    }
+
+    @media screen and (min-width: 1280px){
+        width: 100%;
+        height: 240px;
+    }
 `;
 
 export const ImageUrlBanner = styled.img``;
@@ -43,18 +63,58 @@ export const VacanciesTitle = styled.h1`
     font-size: 2rem;
     font-weight: bold;
     color: ${Colors.primaryColor};
+
+    @media screen and (min-width: 768px){
+        font-size: 4rem;
+    }
+
+    @media screen and (min-width: 1024px){
+        font-size: 3rem;
+    }
+
+    @media screen and (min-width: 1280px){
+        font-size: 2.5rem;
+    }
 `;
 
 export const CompanyName = styled.h2`
     font-size: 17px;
     font-weight: bold;
     color: ${Colors.darkGrayColor};
+
+    @media screen and (min-width: 768px){
+        font-size: 30px;
+    }
+
+    @media screen and (min-width: 1024px){
+        font-size: 23px;
+    }
+
+    @media screen and (min-width: 1280px){
+        font-size: 17px;
+    }
 `;
 
 export const Description = styled.p`
+    font-size: 1rem;
     text-align: center;
     margin: 0 0 20px 0;
-    width: 45%;
+    width: 95%;
+
+    @media screen and (min-width: 768px){
+        width: 90%;
+        font-size: 1.5rem;
+    }
+
+    @media screen and (min-width: 1024px){
+        width: 75%;
+        font-size: 1.5rem;
+    }
+
+    @media screen and (min-width: 1280px){
+        width: 55%;
+        font-size: 1.2rem;
+    }
 `;
 
 export const Wage = styled.h3`
@@ -66,4 +126,9 @@ export const Wage = styled.h3`
     font-weight: bold;
     color: ${Colors.darkGrayColor};
     box-shadow: ${Colors.boxShadowStyle};
+
+    @media screen and (min-width: 768px){
+        width: 150px;
+        font-size: 30px;
+    }
 `;
