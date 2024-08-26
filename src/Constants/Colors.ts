37d6c5e8cc51c1e6e3d307e3c1ae0f8493ec8b24
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-25 22:12:12 -0300

    Create boxShadow constant!

--- src/Constants/Colors.ts
+++ src/Constants/Colors.ts
@@ -4,6 +4,7 @@ const bgColor: string = "#182625";
 const lightGrayColors: string = "#455559";
 const darkGrayColor: string = "#2F3D40";
 const whiteColor: string = "white";
+const boxShadowStyle: string = "10px 10px 10px rgba(0, 0, 0, 0.1)";
 
 export {
     primaryColor,
@@ -12,4 +13,5 @@ export {
     lightGrayColors,
     darkGrayColor,
     whiteColor,
+    boxShadowStyle
 }
