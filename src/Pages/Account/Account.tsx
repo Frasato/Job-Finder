2872668df900ae2761840adfd8fe33f9266871c4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-28 11:04:25 -0300

    Get vacancies from db and create only for this user

--- src/Pages/Account/Account.tsx
+++ src/Pages/Account/Account.tsx
@@ -5,14 +5,56 @@ import { VacanciesContainerHiring, VacanciesContainerWork } from "./Account.styl
 import Button from "../../Components/Button/Button";
 import { Navigate } from "react-router";
 import Separator from "../../Components/Separator/Separator";
+import { get, ref } from "firebase/database";
+import { db } from "../../firebase/firebase";
+import { VacanciesType } from "../../@Types/VacanciesType";
+import Vacancies from "../../Components/Vacancies/Vacancies";
 
 const Account = () =>{
 
-    const {interest} = useContext(Context);
+    const {interest, username} = useContext(Context);
+    const [localVacancies, setLocalVacancies] = useState<VacanciesType[]>([]);
     const [btnClick, setBtnClick] = useState<boolean>(false);
+    const [load, setLoad] = useState<boolean>(false);
 
     useEffect(()=>{
         setBtnClick(false);
+        setLoad(true);
+        const getLocalVacancies = async() =>{
+
+            try{
+
+                const dbRef = ref(db, `vacancies/`);
+                const responses = await get(dbRef);
+                   
+                const newVacancies: VacanciesType[] = [];
+
+                responses.forEach((vacancie) => {
+                    const response = vacancie.val();
+                    if(response.createdBy == username?.username.toLowerCase()){
+                        const vacancieModel = {
+                            id: response.id,
+                            vacanciesTitle: response.title,
+                            companyName: response.companyname,
+                            description: response.description,
+                            wage: response.wage,
+                        }
+
+                        newVacancies.unshift(vacancieModel);
+                    }
+                });
+
+                setLocalVacancies(newVacancies);
+
+
+            }catch(err){
+                console.error(err);
+            }
+
+        }
+
+        getLocalVacancies();
+        setLoad(false)
     },[])
 
     if(btnClick){
@@ -31,7 +73,15 @@ const Account = () =>{
                 <VacanciesContainerHiring>
                     <Button textButton="Create" onClick={() => setBtnClick(true)}/>
                         <Separator />
-                    {/* Vagas que a empresa tem em aberto */}
+                    {localVacancies.length != 0?
+                        localVacancies.map((item, index)=>{
+                            return(
+                                <Vacancies vacanciesTitle={item.vacanciesTitle} companyName={item.companyName} description={item.description} wage={item.wage} />
+                            )
+                        })
+                        :
+                        <></>
+                    }
                 </VacanciesContainerHiring>
             }
         </>
